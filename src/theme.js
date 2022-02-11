ad839ce92b00364a3c18f6ecb1450516bd7e07ed
fabianrguez <fabian.rguezg@gmail.com>
2022-02-11 15:13:29 +0000

    rename priority

--- src/theme.js
+++ src/theme.js
@@ -11,7 +11,7 @@ export const appTheme = {
   avatarBackground: '#e8e8e8',
   priorityBadge: {
     color: {
-      veryHigh: '#ff007c',
+      asap: '#ff007c',
       high: '#d976a6',
       low: '#82aafa',
       normal: '#77d3cf',
